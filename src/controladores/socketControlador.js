c7daf3752e2ad34c23b8b4302c597136b58c9e05
Fabian <fabig4da@gmail.com>
2022-03-04 13:31:29 -0500

    db conection

---
+++ src/controladores/socketControlador.js
@@ -0,0 +1,6 @@
+module.exports = {
+    socketContolador: (socket)=>{
+        socket.emit('con','conected');
+
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const {Server} = require('socket.io');
+const { Server } = require('socket.io');
 const http = require('http');
 const cors = require('cors');
-const path =require('path');
+const path = require('path');
+const mongoose = require('mongoose');
 
 const rutas = require('./rutas');
+const { socketContolador } = require('./controladores/socketControlador');
 
 
 
@@ -14,20 +16,31 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+
+mongoose.connect('mongodb://localhost:27017/test', (error) => {
+    if (error)
+        return console.log('error al conectar la base de datos');
+    console.log('base de datos conectada satisfatoriamente');
+});
+
+
+
 app.use(cors());
 app.use(express.static('publicos'))
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
+io.on('connection', (socket)=>{
+    socketContolador(socket);
+})
 
 
 app.use('/api', rutas);
-app.use('/login', (req, res)=>{
+app.use('/login', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../vistas/login.html'))
 })
 
 
 
-server.listen(port, ()=>console.log('servidor iniciado en el perto: '+ port))
+server.listen(port, () => console.log('servidor iniciado en el perto: ' + port))
 
